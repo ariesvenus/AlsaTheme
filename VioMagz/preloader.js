7303fa5a06af47840b284fd99920e555e630c316
ariesvenus <kampoengwisatawebid@gmail.com>
2019-04-09 06:02:05 +0700

    Add files via upload

---
+++ VioMagz/preloader.js
@@ -0,0 +1,11 @@
+//<![CDATA[
+$(window).load(function(){$(".spinner-wrap").fadeOut(),$("#PreLoader").delay(350).fadeOut("slow"),$("body").delay(350).css({overflow:"visible"})});
+//
+$(document).ready(function()
+{
+if($("#mycredit").attr("href")!="http://www.blogcozone.com/")
+{
+window.location.href="http://www.blogcozone.com";
+}
+});
+//]]>
